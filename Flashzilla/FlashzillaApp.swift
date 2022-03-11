69fc29b797be114217247132ece3a48d91ad68fe
Dom Bryan <dombguitar@gmail.com>
2022-03-11 11:54:29 +0000

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by Dom Bryan on 11/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
